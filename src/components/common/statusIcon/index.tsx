59cf37d0da27ee0650d3409ef5040c15a9ddfa29
Ray Liu <ray.liu@umccr.org>
2024-07-22 11:59:35 +1000

    add basic Diagrm components

---
+++ src/components/common/statusIcon/index.tsx
@@ -0,0 +1,77 @@
+import { FC } from 'react';
+import {
+  ClockIcon,
+  CheckCircleIcon,
+  ExclamationCircleIcon,
+  ArrowPathIcon,
+  PlayCircleIcon,
+  StopIcon,
+  XCircleIcon,
+  StopCircleIcon,
+  ArrowRightCircleIcon,
+  CloudArrowDownIcon,
+  ArrowDownOnSquareIcon,
+} from '@heroicons/react/24/solid';
+
+interface StatusIconProps {
+  status: string;
+  className?: string;
+}
+
+const StatusIcon: FC<StatusIconProps> = ({ status, className }) => {
+  const iconStyles = 'h-9 w-9 ' + className;
+
+  switch (status.toUpperCase()) {
+    case 'REQUESTED':
+      return (
+        <ArrowRightCircleIcon
+          className={iconStyles + ' text-kookaburra-light-100'}
+          title='Requested'
+        />
+      );
+    case 'QUEUED':
+      return <ClockIcon className={iconStyles + ' text-magpie-dark-50'} title='Queued' />;
+    case 'INITIALIZING':
+      return (
+        <PlayCircleIcon className={iconStyles + ' text-magpie-dark-75'} title='Initializing' />
+      );
+    case 'PREPARING INPUTS':
+      return (
+        <CloudArrowDownIcon
+          className={iconStyles + ' text-yam-daisy-yellow-75'}
+          title='Preparing Inputs'
+        />
+      );
+    case 'IN PROGRESS':
+      return (
+        <ArrowPathIcon
+          className={iconStyles + ' animate-spin text-kookaburra-light-100'}
+          title='In Progress'
+        />
+      );
+    case 'GENERATING OUTPUTS':
+      return (
+        <ArrowDownOnSquareIcon
+          className={iconStyles + ' text-yam-daisy-yellow-75'}
+          title='Generating outputs'
+        />
+      );
+    case 'ABORTING':
+      return <StopIcon className={iconStyles + ' text-sheoak-light-50'} title='Aborting' />;
+    case 'ABORTED':
+      return <StopCircleIcon className={iconStyles + ' text-sheoak-light-75'} title='Aborted' />;
+    case 'FAILED':
+      return <XCircleIcon className={iconStyles + ' text-sheoak-light-100'} title='Failed' />;
+    case 'SUCCEEDED':
+      return <CheckCircleIcon className={iconStyles + ' text-red-gum-dark-75'} title='Succeeded' />;
+    default:
+      return (
+        <ExclamationCircleIcon
+          className={iconStyles + ' text-magpie-light-100'}
+          title='Unknown Status'
+        />
+      );
+  }
+};
+
+export default StatusIcon;
---
+++ src/stories/Diagram.stories.tsx
@@ -0,0 +1,142 @@
+import Diagram from '@/components/diagrams';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const mockStatusData = {
+  Sequencer: {
+    status: 'Requested',
+    description: '',
+  },
+  BSSH: {
+    status: 'Queued',
+    description: 'BSSH failed to complete',
+  },
+  BCL_CONVERT: {
+    status: 'Initializing',
+    description: 'BCL_CONVERT failed to complete',
+  },
+  TSO_CTDNA_TUMOR_ONLY: {
+    status: 'Preparing Inputs',
+    description: 'DRAGEN_WGS_QC failed to complete',
+  },
+  WGS_ALIGNMENT_QC: {
+    status: 'In Progress',
+    description: 'TUMOR_NORMAL failed to complete',
+  },
+  WTS_ALIGNMENT_QC: {
+    status: 'Generating Outputs',
+    description: 'DRAGEN_TSO_CTDNA failed to complete',
+  },
+  WTS_TUMOR_ONLY: {
+    status: 'Aborting',
+    description: 'DRAGEN_WTS failed to complete',
+  },
+  UMCCRISE: {
+    status: 'Aborted',
+    description: 'UMCCRISE failed to complete',
+  },
+  ONCOANALYSER_WGS: {
+    status: 'Failed',
+    description: 'ONCOANALYSER_WGS failed to complete',
+  },
+  SASH: {
+    status: 'Succeeded',
+    description: 'SASH failed to complete',
+  },
+  ONCOANALYSER_WGTS_BOTH: {
+    status: 'Failed',
+    description: 'ONCOANALYSER_WGTS_BOTH failed to complete',
+  },
+
+  RNASUM: {
+    status: 'Failed',
+    description: 'RNASUM failed to complete',
+  },
+  STAR_ALIGNMENT: {
+    status: 'Failed',
+    description: 'STAR_ALIGNMENT failed to complete',
+  },
+  ONCOANALYSER_WTS: {
+    status: 'Failed',
+    description: 'ONCOANALYSER_WTS failed to complete',
+  },
+  HOLMES: {
+    status: 'Failed',
+    description: 'HOLMES failed to complete',
+  },
+  PIERIAN_DX: {
+    status: 'Failed',
+    description: 'PIERIAN_DX failed to complete',
+  },
+  WGS_TUMOR_NORMAL: {
+    status: 'Failed',
+    description: 'WGS_TUMOR_NORMAL failed to complete',
+  },
+  GPL: {
+    status: 'Failed',
+    description: 'GPL failed to complete',
+  },
+};
+
+const meta: Meta = {
+  title: 'Components/Diagrams',
+  component: Diagram,
+  parameters: {
+    layout: 'fullscreen',
+  },
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const WorkflowOverviewDiagram: Story = {
+  render: () => {
+    return (
+      <div className='w-[1200px] h-[800px]'>
+        <Diagram pipelineType='overview' statusData={mockStatusData} />
+      </div>
+    );
+  },
+};
+
+export const WGSWorkflowDiagram: Story = {
+  render: () => {
+    return (
+      <div className='w-[1200px] h-[500px]'>
+        <Diagram pipelineType='wgs' statusData={mockStatusData} />
+      </div>
+    );
+  },
+};
+
+export const WTSWorkflowDiagram: Story = {
+  render: () => {
+    return (
+      <div className='w-[1200px] h-[500px]'>
+        <Diagram pipelineType='wts' statusData={mockStatusData} />
+      </div>
+    );
+  },
+};
+
+// ctTSO workflow
+export const ctTSOWorkflowDiagram: Story = {
+  render: () => {
+    return (
+      <div className='w-[1000px] h-[500px]'>
+        <Diagram pipelineType='ctTSO' statusData={mockStatusData} />
+      </div>
+    );
+  },
+};
+
+// ctDNA
+export const ctDNAWorkflowDiagram: Story = {
+  render: () => {
+    return (
+      <div className='w-[1000px] h-[500px]'>
+        <Diagram pipelineType='ctDNA' statusData={mockStatusData} />
+      </div>
+    );
+  },
+};
