4bdc6f881b6e3935fd6c4a6dad8e4a8648d3759c
Ray Liu <ray.liu@umccr.org>
2024-10-30 12:02:57 +1100

    refactor detail page and debug some wfr issue

--- src/modules/runs/Pages/AnalysisRunsDetails.tsx
+++ src/modules/runs/Pages/AnalysisRunsDetails.tsx
@@ -1,12 +1,19 @@
 import AnalysisRunDetailsTable from '../components/analysisRuns/AnalysisRunDetailsTable';
 import AnalysisRunDetailsWorkflowRuns from '../components/analysisRuns/AnalysisRunDetailsWorkflowRuns';
+import { ContentTabs } from '@/components/navigation/tabs';
+
 const AnalysisRunsDetails = () => {
-  return (
-    <div>
-      <AnalysisRunDetailsTable />
-      <AnalysisRunDetailsWorkflowRuns />
-    </div>
-  );
+  const tabs = [
+    {
+      label: 'Analysis Context',
+      content: <AnalysisRunDetailsTable />,
+    },
+    {
+      label: 'Workflow Runs',
+      content: <AnalysisRunDetailsWorkflowRuns />,
+    },
+  ];
+  return <ContentTabs tabs={tabs} />;
 };
 
 export default AnalysisRunsDetails;
