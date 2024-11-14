b5c9b9732da67427cef5ba8a0de2a1b3d9686006
Ray Liu <ray.liu@umccr.org>
2024-11-14 13:16:41 +1100

    refactor page container

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -11,12 +11,14 @@ const MainLayout = ({ children }: PropsWithChildren) => {
     <div className='h-screen w-screen flex flex-col'>
       <Header className='py-1 fixed' />
       {/* `mt-12` because the header above will take space h-10 + my-1 */}
-      <div className='bg-heritage-blue-100 flex flex-1 mt-12'>
+      <div className='bg-heritage-blue-100 flex flex-1 mt-12 overflow-hidden'>
         <SideNavbar navigation={navigation} className='fixed h-full w-14' />
         {/* ml-14 because the <SideNavBar /> have width of w-14 */}
-        <div className='ml-14 w-full h-full flex-auto overflow-auto rounded-tl-2xl bg-white'>
+        <div className='ml-14 w-full flex-auto overflow-hidden rounded-tl-2xl bg-white'>
           {/* <Breadcrumb /> */}
-          {children || <Outlet />}
+          <div className='flex flex-col flex-1 w-full h-full overflow-auto'>
+            {children || <Outlet />}
+          </div>
         </div>
       </div>
     </div>
