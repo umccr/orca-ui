c7792ad328c63ef0e19a1379839679d549d449d5
william <william.intan@unimelb.edu.au>
2025-07-14 16:41:35 +1000

    upgrade storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,21 +4,20 @@ import path from 'path';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+
   addons: [
     '@storybook/addon-links',
-    '@storybook/addon-essentials',
     '@chromatic-com/storybook',
-    '@storybook/addon-interactions',
     'storybook-addon-remix-react-router',
     '@storybook/addon-onboarding',
+    '@storybook/addon-docs',
   ],
+
   framework: {
-    name: '@storybook/react-vite',
+    name: '@storybook/react',
     options: {},
   },
-  docs: {
-    autodocs: 'tag',
-  },
+
   viteFinal: async (config) => {
     return mergeConfig(config, {
       resolve: {
