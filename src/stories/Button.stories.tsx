bdc45046329e91edac336b7fee4c84d603cc7d1d
william <william.intan@unimelb.edu.au>
2025-07-14 17:43:00 +1000

    type check

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { fn } from '@storybook/test';
+import { fn } from 'storybook/test';
 import { Button, IconButton } from '@/components/common/buttons';
 import { ButtonProps } from '@/components/common/buttons/Button';
 import { IconButtonProps } from '@/components/common/buttons/IconButton';
