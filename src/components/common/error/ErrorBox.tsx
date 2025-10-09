0d9d1a160823655b9407f88e541af5f9eb483b9a
William Intan <61998484+williamputraintan@users.noreply.github.com>
2025-10-09 18:32:54 +1100

    Fix: Error Handling on File Listing (#209)

---
+++ src/components/common/error/ErrorBox.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type Props = { message: string };
+
+export default function ErrorBox({ message }: Props) {
+  return (
+    <div className='rounded-md bg-red-50 p-4 dark:bg-red-900/10'>
+      <pre className='font-mono text-sm break-words whitespace-pre-wrap text-red-600 dark:text-red-400'>
+        {message}
+      </pre>
+    </div>
+  );
+}
